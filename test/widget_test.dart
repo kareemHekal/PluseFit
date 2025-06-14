edeb263982357cba5db1c5913f35af89a3f327bd
East-Sound <kareemhekal19@gmail.com>
2025-06-15 02:21:39 +0300

    add the test again

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('My widget test', (WidgetTester tester) async {
+    // هنا كود الاختبار بتاعك
+    expect(1 + 1, equals(2));
+  });
+}
