0ba521434404f4859e611359948e9938af97253b
اسمك <ahmedbasha01208@gmail.com>
2025-06-15 16:20:15 +0300

    feat():change text style

--- lib/core/utils/text_style_manager.dart
+++ lib/core/utils/text_style_manager.dart
@@ -2,63 +2,68 @@ import 'package:flutter/material.dart';
 
 class AppTextStyle {
   static const TextStyle regular12 = TextStyle(
-    fontFamily: 'Roboto',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w400,
     fontSize: 12,
   );
 
   static const TextStyle regular14 = TextStyle(
-    fontFamily: 'Inter',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w400,
     fontSize: 14,
   );
   static const TextStyle regular16 = TextStyle(
-    fontFamily: 'Inter',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w400,
     fontSize: 16,
   );
   static const TextStyle medium14 = TextStyle(
-    fontFamily: 'Roboto',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w500,
     fontSize: 14,
   );
   static const TextStyle medium16 = TextStyle(
-    fontFamily: 'Roboto',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w500,
     fontSize: 16,
   );
   static const TextStyle medium18 = TextStyle(
-      fontFamily: 'Roboto',
+      fontFamily: 'Baloo Thambi 2',
       fontWeight: FontWeight.w500,
       fontSize: 18,
       color: Colors.black);
 
   static const TextStyle medium20 = TextStyle(
-    fontFamily: 'Inter',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w500,
     fontSize: 20,
   );
 
   static const TextStyle regular25 = TextStyle(
-    fontFamily: 'SF Pro',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w400,
     fontSize: 25,
   );
 
   static const TextStyle regular24 = TextStyle(
-    fontFamily: 'Roboto',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w400,
     fontSize: 24,
   );
+  static const TextStyle regular18 = TextStyle(
+    fontFamily: 'Baloo Thambi 2',
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+  );
 
   static const TextStyle semiBold12 = TextStyle(
-    fontFamily: 'Inter',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w600,
     fontSize: 12,
   );
 
   static const TextStyle semiBold20 = TextStyle(
-    fontFamily: 'Inter',
+    fontFamily: 'Baloo Thambi 2',
     fontWeight: FontWeight.w600,
     fontSize: 20,
   );
--- lib/ui/Auth/register/register_view.dart
+++ lib/ui/Auth/register/register_view.dart
@@ -1,4 +1,6 @@
+import 'package:fit_zone/core/resuable_comp/auth_background_cuver.dart';
 import 'package:fit_zone/core/utils/assets_manager.dart';
+import 'package:fit_zone/core/utils/text_style_manager.dart';
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatelessWidget {
@@ -6,19 +8,21 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(AssetsManager.imagesAuthBackground),
-          fit: BoxFit.cover,
-        ),
-        color: Colors.transparent,
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          children: [],
-        ),
+    return AuthBackgroundCuver(
+      widget: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/icon/app_icon.png"),
+          Text(
+            "Hey There",
+            style: AppTextStyle.regular18.copyWith(color: Colors.white),
+          ),
+          Text(
+            "create an account",
+            style: AppTextStyle.semiBold20.copyWith(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
