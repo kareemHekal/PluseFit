d1d205378df6cf9787a39a01564fc5600a087553
اسمك <ahmedbasha01208@gmail.com>
2025-06-15 15:14:55 +0300

    change assets manger

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,5 +1,23 @@
-// ignore_for_file: prefer_single_quotes
 class AssetsManager {
-  static const String backgroundCard = "assets/images/bg.png";
-  static const String imagePolyline = "assets/images/icons8-motorbike-96.png";
+  /// Assets for imagesAuthBackground
+  /// assets/images/auth_background.png
+  static const String imagesAuthBackground =
+      "assets/images/auth_background.png";
+
+  /// Assets for imagesIconAppIcon
+  /// assets/images/icon/app_icon.png
+  static const String imagesIconAppIcon = "assets/images/icon/app_icon.png";
+
+  /// Assets for imagesIconAppIconForeground
+  /// assets/images/icon/app_icon_foreground.png
+  static const String imagesIconAppIconForeground =
+      "assets/images/icon/app_icon_foreground.png";
+
+  /// Assets for translationsAr
+  /// assets/translations/ar.json
+  static const String translationsAr = "assets/translations/ar.json";
+
+  /// Assets for translationsEn
+  /// assets/translations/en.json
+  static const String translationsEn = "assets/translations/en.json";
 }
