e8fc0602fa23ec3585851d251ab969232f956b90
اسمك <ahmedbasha01208@gmail.com>
2025-07-01 14:36:00 +0300

    feat(firebase):sync project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_zone/firebase_options.dart';
 import 'package:fit_zone/ui/Auth/view_model/cubit/auth_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,6 +17,10 @@ import 'my_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
   Hive.registerAdapter(ConversationModelAdapter());
   Hive.registerAdapter(MessageModelAdapter());
